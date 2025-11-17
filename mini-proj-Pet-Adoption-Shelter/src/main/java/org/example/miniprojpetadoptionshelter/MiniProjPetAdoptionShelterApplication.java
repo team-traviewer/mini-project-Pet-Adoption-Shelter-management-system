dba7a9a74f93fafc94ee8fc6ab6d8de1bfa60a8e
ehdgn <ehdgn6570@gmail.com>
2025-11-17 11:32:27 +0900

    proj-create

---
+++ mini-proj-Pet-Adoption-Shelter/src/main/java/org/example/miniprojpetadoptionshelter/MiniProjPetAdoptionShelterApplication.java
@@ -0,0 +1,13 @@
+package org.example.miniprojpetadoptionshelter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjPetAdoptionShelterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniProjPetAdoptionShelterApplication.class, args);
+    }
+
+}
