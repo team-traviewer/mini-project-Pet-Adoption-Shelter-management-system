3d41a4f26f75bcf826f2d5f9fa6d6d028313ed1e
kim-se-hooh <wwb8524@naver.com>
2025-11-21 17:23:37 +0900

    feaure/entity-dto-service

--- mini-proj-Pet-Adoption-Shelter/src/main/java/org/example/miniprojpetadoptionshelter/entity/base/BaseTimeEntity.java
+++ mini-proj-Pet-Adoption-Shelter/src/main/java/org/example/miniprojpetadoptionshelter/entity/base/BaseTimeEntity.java
@@ -1,13 +1,27 @@
 package org.example.miniprojpetadoptionshelter.entity.base;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
 
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false,
+            columnDefinition = "DATETIME(6)")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = true,
+            columnDefinition = "DATETIME(6)")
+    private LocalDateTime updatedAt;
 }
---
+++ mini-proj-Pet-Adoption-Shelter/src/main/java/org/example/miniprojpetadoptionshelter/repository/AnimalRepository.java
@@ -0,0 +1,6 @@
+package org.example.miniprojpetadoptionshelter.repository;
+
+import org.example.miniprojpetadoptionshelter.entity.Animal;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnimalRepository extends JpaRepository<Animal, Long> {}
