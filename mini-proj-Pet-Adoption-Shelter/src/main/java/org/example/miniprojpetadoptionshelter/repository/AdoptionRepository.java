965d3427515c6cee20dfa86789178e3f6a8caff9
김세훈 <wwb8524@naver.com>
2025-11-24 02:28:17 +0900

    fix/adoption

--- mini-proj-Pet-Adoption-Shelter/src/main/java/org/example/miniprojpetadoptionshelter/repository/AdoptionRepository.java
+++ mini-proj-Pet-Adoption-Shelter/src/main/java/org/example/miniprojpetadoptionshelter/repository/AdoptionRepository.java
@@ -1,12 +1,11 @@
 package org.example.miniprojpetadoptionshelter.repository;
 
 import org.example.miniprojpetadoptionshelter.entity.adoptions.Adoption;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface AdoptionRepository extends JpaRepository<Adoption, Long> {
-    @EntityGraph(attributePaths = {"animal", "application", "adopter"})
-    Optional<Adoption> findById(Long id);
 }
