446aa210ba8ce94f90a078d1d6b8cfeeae645add
untilthe-end <letsgohangun@gmail.com>
2025-11-27 23:47:43 +0900

    foster controller update

--- mini-proj-Pet-Adoption-Shelter/src/main/java/org/example/miniprojpetadoptionshelter/controller/fromAnimal/FosterController.java
+++ mini-proj-Pet-Adoption-Shelter/src/main/java/org/example/miniprojpetadoptionshelter/controller/fromAnimal/FosterController.java
@@ -1,21 +1,25 @@
 package org.example.miniprojpetadoptionshelter.controller.fromAnimal;
 
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
 import org.example.miniprojpetadoptionshelter.common.apis.fromAnimal.FosterApi;
+import org.example.miniprojpetadoptionshelter.common.enums.FosterStatus;
 import org.example.miniprojpetadoptionshelter.dto.ResponseDto;
+import org.example.miniprojpetadoptionshelter.dto.fromAnimal.foster.request.FosterCancelReq;
+import org.example.miniprojpetadoptionshelter.dto.fromAnimal.foster.request.FosterCloseReq;
 import org.example.miniprojpetadoptionshelter.dto.fromAnimal.foster.request.FosterCreateReq;
-import org.example.miniprojpetadoptionshelter.dto.fromAnimal.foster.request.FosterUpdateReq;
 import org.example.miniprojpetadoptionshelter.dto.fromAnimal.foster.response.FosterDetailRes;
 import org.example.miniprojpetadoptionshelter.dto.fromAnimal.foster.response.FosterListRes;
 import org.example.miniprojpetadoptionshelter.security.user.UserPrincipal;
 import org.example.miniprojpetadoptionshelter.service.fromAnimal.FosterService;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -26,8 +30,8 @@ public class FosterController {
     @PreAuthorize("hasRole('STAFF')")
     @PostMapping(FosterApi.ROOT)
     public ResponseEntity<ResponseDto<Void>> createFoster(
-            @RequestBody FosterCreateReq req,
-            @AuthenticationPrincipal UserPrincipal principal
+            @AuthenticationPrincipal UserPrincipal principal,
+            @RequestBody FosterCreateReq req
     ) {
         ResponseDto<Void> response = fosterService.createFoster(principal, req);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
@@ -36,30 +40,47 @@ public class FosterController {
     @PreAuthorize("hasAnyRole('STAFF','ADMIN')")
     @GetMapping(FosterApi.ROOT)
     public ResponseEntity<ResponseDto<List<FosterListRes>>> getFosterList(
-            @RequestParam Long animalId,
-            @AuthenticationPrincipal UserPrincipal principal
+            @AuthenticationPrincipal UserPrincipal principal,
+            @RequestParam (required = false) Long fosterUserId,
+            @RequestParam (required = false) FosterStatus status,
+            @RequestParam (required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate from,
+            @RequestParam (required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate to
     ) {
-        ResponseDto<List<FosterListRes>> response = fosterService.getFosterList(animalId, principal);
+        ResponseDto<List<FosterListRes>> response = fosterService.getFosterList(principal, fosterUserId, status, from, to);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @PreAuthorize("hasAnyRole('STAFF','ADMIN')")
     @GetMapping(FosterApi.BY_ID)
     public ResponseEntity<ResponseDto<FosterDetailRes>> getFosterDetail(
-            @PathVariable Long id,
+            @PathVariable("fosterId") @Positive(message = "Id는 1이상이어야 합니다.") Long fosterId,
             @AuthenticationPrincipal UserPrincipal principal
     ) {
-        ResponseDto<FosterDetailRes> response = fosterService.getFosterDetail(id, principal);
+        ResponseDto<FosterDetailRes> response = fosterService.getFosterDetail(fosterId, principal);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @PreAuthorize("hasAnyRole('STAFF','ADMIN')")
-    @PutMapping(FosterApi.BY_ID)
-    public ResponseEntity<ResponseDto<Void>> updateFoster(
-            @PathVariable Long id, @RequestBody FosterUpdateReq req,
-            @AuthenticationPrincipal UserPrincipal principal
+    @PutMapping(FosterApi.CLOSE)
+    public ResponseEntity<ResponseDto<Void>> closeFoster(
+            @PathVariable("fosterId") @Positive(message = "Id는 1이상이어야 합니다.") Long fosterId,
+            @AuthenticationPrincipal UserPrincipal principal,
+            @RequestBody FosterCloseReq req
     ) {
-        ResponseDto<Void> response = fosterService.updateFoster(id, req, principal);
+        ResponseDto<Void> response = fosterService.closeFoster(fosterId, principal, req);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @PreAuthorize("hasAnyRole('STAFF','ADMIN')")
+    @PutMapping(FosterApi.CANCEL)
+    public ResponseEntity<ResponseDto<Void>> cancelFoster(
+            @PathVariable("fosterId") @Positive(message = "Id는 1이상이어야 합니다.") Long fosterId,
+            @AuthenticationPrincipal UserPrincipal principal,
+            @RequestBody FosterCancelReq req
+    ) {
+        ResponseDto<Void> response = fosterService.cancelFoster(fosterId, principal, req);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 }
--- mini-proj-Pet-Adoption-Shelter/src/main/java/org/example/miniprojpetadoptionshelter/dto/fromAnimal/foster/response/FosterDetailRes.java
+++ mini-proj-Pet-Adoption-Shelter/src/main/java/org/example/miniprojpetadoptionshelter/dto/fromAnimal/foster/response/FosterDetailRes.java
@@ -1,17 +1,41 @@
 package org.example.miniprojpetadoptionshelter.dto.fromAnimal.foster.response;
 
 import org.example.miniprojpetadoptionshelter.common.enums.FosterStatus;
+import org.example.miniprojpetadoptionshelter.common.utils.DateUtils;
+import org.example.miniprojpetadoptionshelter.entity.fromAnimal.Foster;
 
 import java.time.LocalDate;
 
+//@JsonInclude(JsonInclude.Include.NON_NULL)
+// 테스팅 위해 endDate: null 을 보고싶으면 @JsonInclude 는 달지마라
 public record FosterDetailRes(
         Long id,
         Long animalId,
+        String animalSpecies,
+        String animalBreed,
         Long fosterUserId,
         LocalDate startDate,
         LocalDate endDate,
         FosterStatus status,
         String note,
-        LocalDate createdAt
+        String createdAtKst,
+        String createdUtcIso
 ) {
+    public static FosterDetailRes from(Foster foster) {
+        if (foster == null) return null;
+
+        return new FosterDetailRes(
+                foster.getId(),
+                foster.getAnimal().getId(),
+                foster.getAnimal().getSpecies(),
+                foster.getAnimal().getBreed() != null ? foster.getAnimal().getBreed() : "unknown",
+                foster.getFosterUser().getId(),
+                foster.getStartDate(),
+                foster.getEndDate(),
+                foster.getStatus(),
+                foster.getNote(),
+                DateUtils.toKstString(foster.getCreatedAt()),
+                DateUtils.toUtcString(foster.getCreatedAt())
+        );
+    }
 }
